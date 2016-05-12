72eb6d3554115d11a9eabd99b4edff946e3b9f36
Zack Smith <z.smith32@gmail.com>
2016-05-12 11:26:16 -0700

    total earnings stacked column graph with date slider operational.  Checking if I can ommit dates with 0 data from chart

--- public/javascripts/dashboard_client.js
+++ public/javascripts/dashboard_client.js
@@ -1,22 +1,25 @@
-var numDays = 30
-var dateArray = getDates(numDays)
-var sampleDay = moment('03/25/2016', 'MM/DD/YYYY')
 
+// ___________ data parsing ____________
 
+var firstDay = allJobs[0]['jobStart']
+
+// parse JSON data for chart
+var dateArray = getDates(firstDay)
 var jobsByDay = dateArray.map(filterJobs)
 var totalByDay = jobsByDay.map(sumJobs)
 var chartData = addDate(dateArray, totalByDay)
-
-chartData.unshift(['Earnings', 'Payout', 'Tips'])
+chartData.unshift(['Date', 'Payout', 'Tips'])
 console.log(chartData)
 
+// functions for parsing data for chart
 
-
-function getDates (numDays) {
+function getDates (firstDay) {
 	var endDay = moment()
-	var startDay = moment().subtract(numDays, 'days')
+	var startDay = moment(firstDay)
 	var dateArray = []
 	
+	// need to add a filter for if first shift is in this day, then all
+	// jobs from that shift are on this day... maybee
 	while (startDay < endDay) {
 		dateArray.push(moment(startDay))
 		startDay = startDay.add(1, 'd')
@@ -51,7 +54,7 @@ function addDate(dateArray, dataArray) {
 	var makeReady = []
 
 	for (var i = 0; i<dataArray.length; i++) {
-		makeReady.push([dateArray[i].format('M/D'), 
+		makeReady.push([new Date(dateArray[i].format('YYYY,MM,DD')), 
 										dataArray[i][0], 
 										dataArray[i][1]])
 	}
@@ -59,10 +62,48 @@ function addDate(dateArray, dataArray) {
 }
 
 
+// ______________ load google chart __________________
 
+google.charts.load('current', {'packages':['corechart', 'controls']});
+  google.charts.setOnLoadCallback(drawStuff);
 
+  function drawStuff() {
 
+    var dashboard = new google.visualization.Dashboard(
+      document.getElementById('dash_div'));
 
+    // We omit "var" so that programmaticSlider is visible to changeRange.
+    totalSlider = new google.visualization.ControlWrapper({
+      'controlType': 'DateRangeFilter',
+      'containerId': 'container_div',
+      'options': {
+        'filterColumnLabel': 'Date',
+        'ui': {'labelStacking': 'vertical'}
+      }
+    });
+
+    totalChart  = new google.visualization.ChartWrapper({
+      'chartType': 'ColumnChart',
+      'containerId': 'chart_div',
+      'options': {
+        'width': 900,
+        'height': 300,
+        'isStacked': true,
+        'hAxis': {
+        	'format': 'M/d',
+        	'girdLines': {'count': 10}
+        },
+        'chartArea': {'left': 50, 'top': 50, 'right': 125, 'bottom': 25}
+        //'pieSliceText': 'value'
+    }
+
+  });
+
+  var data = google.visualization.arrayToDataTable( chartData);
+
+  dashboard.bind(totalSlider, totalChart);
+  dashboard.draw(data);
+}
 
 
 
